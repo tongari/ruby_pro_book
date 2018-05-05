07bb5853595f87c23c3b788d8370dfcde78553ec
tongari <info@tongari-as.com>
2018-05-05 21:06:29 +0900

    add folder minitest

---
+++ README.md
@@ -0,0 +1,1 @@
+# ruby_pro_book
---
+++ app/minitest/test/spec_fizz_buzz.rb
@@ -0,0 +1,8 @@
+require './app/minitest/lib/fizz_buzz' 
+
+RSpec.describe 'Fizz Buzz' do
+  example 'fizz_buzz' do
+    expect(fizz_buzz(1)).to eq '1'
+    expect(fizz_buzz(2)).to eq '2'
+  end
+end
