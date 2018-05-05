90cdd716bc4480a0d7ee63550647f68a3bbb6ea2
tongari <info@tongari-as.com>
2018-05-06 00:14:32 +0900

    add foloder 4_loop

---
+++ app/4_loop/test/rgb_test.rb
@@ -0,0 +1,9 @@
+require 'minitest/autorun'
+require './app/4_loop/lib/rgb'
+
+class RgbTest < Minitest::Test
+  def test_to_hex
+    assert_equal '#000000', to_hex(0,0,0)
+    assert_equal '#ffffff', to_hex(255,255,255)
+  end
+end
