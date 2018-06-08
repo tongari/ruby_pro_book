588e7070b871954d220558afc6db85f7c7d64bd6
tongari <info@tongari-as.com>
2018-06-08 23:43:40 +0900

    make rgb test

--- app/4_loop/lib/rgb.rb
+++ app/4_loop/lib/rgb.rb
@@ -1,3 +1,5 @@
+require "bundler/setup"
+require "pry"
 # def to_hex(r,g,b)
 #   hex = '#'
 #   [r,g,b].each do |n|
@@ -10,4 +12,18 @@ def to_hex(r,g,b)
   [r,g,b].inject('#') do |hex,n|
     hex + n.to_s(16).rjust(2, '0')
   end
+end
+
+# def to_ints(hex)
+#   ret = [
+#     hex[1..2].to_i(16),
+#     hex[3..4].to_i(16),
+#     hex[5..6].to_i(16)
+#   ]
+#   # binding.pry
+#   ret
+# end
+
+def to_ints(hex)
+  hex.scan(/\w\w/).map(&:hex)
 end
